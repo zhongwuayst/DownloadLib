2f270182b5b2315d2d1dde2c718c2ba119676536
751890720 <751890720@qq.com>
2015-08-11 22:56:08 +0000

    新文件

---
+++ DownloadProject/DownloadManager.h
@@ -0,0 +1,17 @@
+//
+//  DownloadManager.h
+//  DownloadProject
+//
+//  Created by tiger on 15/8/10.
+//  Copyright (c) 2015年 tiger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadManager : NSObject
+
+-(void) downloadTest;
+
+@end
+
+
---
+++ DownloadProject/DownloadManager.m
@@ -0,0 +1,19 @@
+//
+//  DownloadManager.m
+//  DownloadProject
+//
+//  Created by tiger on 15/8/10.
+//  Copyright (c) 2015年 tiger. All rights reserved.
+//
+
+#import "DownloadManager.h"
+
+@implementation DownloadManager
+
+-(void) downloadTest
+{
+    NSLog(@"下载类");
+}
+
+@end
+
